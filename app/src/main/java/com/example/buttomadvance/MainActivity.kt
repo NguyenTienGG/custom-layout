c24c3686b0e477022279a0acd6636ea0436db48e
tienkk <tiendong2kk2@gmail.com>
2024-07-04 10:05:02 +0700

    done

---
+++ app/src/main/java/com/example/buttomadvance/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.buttomadvance
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.buttomadvance.databinding.ActivityMainBinding
+
+
+private lateinit var binding: ActivityMainBinding
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+    }
+}
